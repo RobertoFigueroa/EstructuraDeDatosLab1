f066f9efd97d8dddc092f9e7ac8c1f07711ab12f
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-01-12 22:49:12 -0600

    Creación del main
    Comencé con del driver del radio ademas agregué los límites para las frecuencias AM y FM

--- untitled/src/CarRadio.java
+++ untitled/src/CarRadio.java
@@ -8,6 +8,11 @@ public class CarRadio {
     private double currentStationAM;
     private double[] savedStationsFM;
     private double[] savedStationsAM;
+    private double minFM = 87.9;
+    private double maxFM = 107.9;
+    private double minAM = 530;
+    private double maxAM = 1610;
+
     //Constructor
     public CarRadio(){
         this.isOn=false;
