bf181384775551c8115e2a9826e8e232cc586cf9
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-01-15 21:47:33 -0600

    Comentarios en la interfaz
    Agregué los comentarios en los metodos de la interfaz

--- untitled/src/Radio.java
+++ untitled/src/Radio.java
@@ -1,10 +1,10 @@
 public interface Radio {
-        public void toggle();
-        public boolean getState();
-        public void changeFrequency();
-        public void changeStation(boolean up);
-        public boolean getFrequency();
-        public void saveStation(int numButton);
-        public void changeStationButton(int numButton);
-        public double getStation();
+        public void toggle();  //enciende el radio
+        public boolean getState();  //devuelve el estado del radio, si esta encendido o apagado
+        public void changeFrequency();  //cambia la frecuencia de AM a FM y vice versa
+        public void changeStation(boolean up);   //cambia la estacion u emisora
+        public boolean getFrequency(); //devuelve la frecuencia en la que se encuentra el radio
+        public void saveStation(int numButton);  //guarda la estacion en determiado boton
+        public void changeStationButton(int numButton);  //cambia la emisora a una que ya este guardada
+        public double getStation();   //devuelve la emisora en la que se encuentra el radio
 }
