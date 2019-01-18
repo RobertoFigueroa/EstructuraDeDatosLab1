a9b7410ce5c08eea3f091cb85ad60ff85347b84f
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-01-18 17:16:59 -0600

    JUnit test
    Agregué la clase de JUnit

---
+++ untitled/src/CarRadioTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+public class CarRadioTest {
+
+    @org.junit.Test
+    public void getState() {
+        
+
+    }
+
+    @org.junit.Test
+    public void getFrequency() {
+    }
+
+    @org.junit.Test
+    public void getStation() {
+    }
+}
