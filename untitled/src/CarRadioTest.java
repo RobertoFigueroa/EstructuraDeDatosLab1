9129ad1e47574c3e4205d6c69e9eabeedb9af2ce
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-01-18 18:09:58 -0600

    Sentencias en Junit y capturas
    En este commit agrego las sentencias para las pruebas de los métodos de JUnit además de las capturas de el JUnit test passed y la captura del programa corriendo con otra clase

--- untitled/src/CarRadioTest.java
+++ untitled/src/CarRadioTest.java
@@ -4,15 +4,25 @@ public class CarRadioTest {
 
     @org.junit.Test
     public void getState() {
-        
+        Radio radio = new CarRadio();
+        radio.toggle();
+        assertTrue(radio.getState());
 
     }
 
     @org.junit.Test
     public void getFrequency() {
+        Radio radio = new CarRadio();
+        assertTrue(radio.getFrequency());
     }
 
     @org.junit.Test
     public void getStation() {
+        Radio radio = new CarRadio();
+        double expected = radio.getStation();
+        radio.changeStation(true);
+        double actual = radio.getStation();
+        assertEquals(expected,actual,radio.getStation());
+
     }
 }
